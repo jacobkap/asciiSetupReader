32707e092320b381072a87e24004a83436a6c618
Jacob Kaplan <jacobkap@sas.upenn.edu>
2017-05-26 10:11:52 -0700

    comment out old code

--- R/SAS_reader.R
+++ R/SAS_reader.R
@@ -1,66 +1,66 @@
-# Will read in fixed-width ASCII files using the
-# SAS setup file
-
-
-
-SAS_reader <- function(ascii_file, SAS_file, fix_values) {
-  text_file <- readr::read_lines(dataset_name)
-  codebook <- readr::read_lines(SAS_file)
-
-}
-
-SAS_proc_codebook <- function(codebook) {
-  proc <- data.frame(codebook[grep("^proc format", codebook,
-                         ignore.case = TRUE):
-                      grep("sas.* infile.*input statem", codebook,
-                           ignore.case = TRUE)],
-                     stringsAsFactors = FALSE)
-  proc[,1] <- gsub("\\' ", "\\'       ", proc[,1])
-  proc[,1] <- gsub("(.*)( .*=.*)", "\\1   \\2", proc[,1])
-  proc <- data.frame(unlist(stringr::str_split(proc, "  |;")),
-                     stringsAsFactors = FALSE)
-  proc <- data.frame(proc[proc[,1] != "",], stringsAsFactors = FALSE)
-  proc[,1] <- gsub("\\'|,", "", proc[,1])
-  proc[,1] <- gsub('\\"', '', proc[,1])
-  proc$value <- gsub("(.*)=.*", "\\1", proc[,1])
-  proc$label <- gsub(".*=(.*)", "\\1", proc[,1])
-  names(proc)[1] <- "column"
-  proc$column <- trimws(proc$column)
-  col <- proc$column[grep("^VALUE", proc$column)[1]]
-  for (i in 1:nrow(proc)) {
-    if (grepl("^VALUE", proc$column[i])) {
-      col <- proc$column[i]
-    }
-    proc$column[i] <- col
-  }
-  proc[,1] <- gsub("value *", "", proc[,1],
-                   ignore.case = TRUE)
-  proc_codebook <- proc[proc$value != proc$label,]
-
-  return(proc_codebook)
-
-}
-# Grabs the column names and numbers. For turning the
-# ASCII file into a data.frame.
-SAS_columns <- function(codebook) {
-  columns <- data.frame(codebook[grep("sas.* infile.*input statem", codebook,
-                                   ignore.case = TRUE):
-                                grep("sas.* label state", codebook,
-                                     ignore.case = TRUE)],
-                        stringsAsFactors = FALSE)
-  columns[,1] <- gsub("(-[0-9]*) ", "\\1   ", columns[,1])
-  columns[,1] <- gsub("\\.[0-9]*", "", columns[,1])
-  columns <- data.frame(unlist(stringr::str_split(columns, "   ")),
-             stringsAsFactors = FALSE)
-  columns <- data.frame(columns[columns[,1] != "",],
-                        stringsAsFactors = FALSE)
-  columns[,1] <- trimws(columns[,1])
-  columns[,1] <- gsub(',|\\$|\\"|\\*', "", columns[,1])
-  columns$first <- gsub(".* (.*)-.*", "\\1", columns[,1])
-  columns$first[grep("-", columns[,1], invert = TRUE)] <-
-                          gsub(".* ([0-9]*)$", "\\1",
-                          columns[grep("-", columns[,1], invert = TRUE), 1])
-
-  columns$second <- gsub(".*-(.*)|", "\\1", columns[,1])
-  columns$name <- gsub("(.*) .*", "\\1", columns[,1])
-}
+# # Will read in fixed-width ASCII files using the
+# # SAS setup file
+#
+#
+#
+# SAS_reader <- function(ascii_file, SAS_file, fix_values) {
+#   text_file <- readr::read_lines(dataset_name)
+#   codebook <- readr::read_lines(SAS_file)
+#
+# }
+#
+# SAS_proc_codebook <- function(codebook) {
+#   proc <- data.frame(codebook[grep("^proc format", codebook,
+#                          ignore.case = TRUE):
+#                       grep("sas.* infile.*input statem", codebook,
+#                            ignore.case = TRUE)],
+#                      stringsAsFactors = FALSE)
+#   proc[,1] <- gsub("\\' ", "\\'       ", proc[,1])
+#   proc[,1] <- gsub("(.*)( .*=.*)", "\\1   \\2", proc[,1])
+#   proc <- data.frame(unlist(stringr::str_split(proc, "  |;")),
+#                      stringsAsFactors = FALSE)
+#   proc <- data.frame(proc[proc[,1] != "",], stringsAsFactors = FALSE)
+#   proc[,1] <- gsub("\\'|,", "", proc[,1])
+#   proc[,1] <- gsub('\\"', '', proc[,1])
+#   proc$value <- gsub("(.*)=.*", "\\1", proc[,1])
+#   proc$label <- gsub(".*=(.*)", "\\1", proc[,1])
+#   names(proc)[1] <- "column"
+#   proc$column <- trimws(proc$column)
+#   col <- proc$column[grep("^VALUE", proc$column)[1]]
+#   for (i in 1:nrow(proc)) {
+#     if (grepl("^VALUE", proc$column[i])) {
+#       col <- proc$column[i]
+#     }
+#     proc$column[i] <- col
+#   }
+#   proc[,1] <- gsub("value *", "", proc[,1],
+#                    ignore.case = TRUE)
+#   proc_codebook <- proc[proc$value != proc$label,]
+#
+#   return(proc_codebook)
+#
+# }
+# # Grabs the column names and numbers. For turning the
+# # ASCII file into a data.frame.
+# SAS_columns <- function(codebook) {
+#   columns <- data.frame(codebook[grep("sas.* infile.*input statem", codebook,
+#                                    ignore.case = TRUE):
+#                                 grep("sas.* label state", codebook,
+#                                      ignore.case = TRUE)],
+#                         stringsAsFactors = FALSE)
+#   columns[,1] <- gsub("(-[0-9]*) ", "\\1   ", columns[,1])
+#   columns[,1] <- gsub("\\.[0-9]*", "", columns[,1])
+#   columns <- data.frame(unlist(stringr::str_split(columns, "   ")),
+#              stringsAsFactors = FALSE)
+#   columns <- data.frame(columns[columns[,1] != "",],
+#                         stringsAsFactors = FALSE)
+#   columns[,1] <- trimws(columns[,1])
+#   columns[,1] <- gsub(',|\\$|\\"|\\*', "", columns[,1])
+#   columns$first <- gsub(".* (.*)-.*", "\\1", columns[,1])
+#   columns$first[grep("-", columns[,1], invert = TRUE)] <-
+#                           gsub(".* ([0-9]*)$", "\\1",
+#                           columns[grep("-", columns[,1], invert = TRUE), 1])
+#
+#   columns$second <- gsub(".*-(.*)|", "\\1", columns[,1])
+#   columns$name <- gsub("(.*) .*", "\\1", columns[,1])
+# }
--- R/old.R
+++ R/old.R
@@ -1,141 +1,141 @@
-# old code
-
-
-value_labels[,1] <- toupper(value_labels[,1])
-
-value_labels[,1] <- gsub('"', "'", value_labels[,1])
-
-value_labels[,1] <-  gsub("\\'(?=-1\\')" , "",
-                          value_labels[,1], perl = TRUE)
-value_labels[,1] <-  gsub("(?<=-1)\\'" , "",
-                          value_labels[,1], perl = TRUE)
-
-
-value_labels[,1] <-  gsub("\\'(?=[0-9+]\\')" , "",
-                          value_labels[,1], perl = TRUE)
-
-value_labels[,1] <- gsub("(?<= [0-9])\\' |(?<=^[0-9])\\' " , " ",
-                         value_labels[,1], perl = TRUE)
-
-value_labels[,1] <- gsub("(?<=[0-9][A-Z])\\'" , "",
-                         value_labels[,1], perl = TRUE,
-                         ignore.case = TRUE)
-
-value_labels[,1] <- gsub("(?<=[0-9][0-9])\\'" , "",
-                         value_labels[,1], perl = TRUE,
-                         ignore.case = TRUE)
-value_labels[,1] <-
-  gsub("\\'(?=[0-9][0-9]\\')|\\'(?=[0-9][0-9])" , "",
-       value_labels[,1], perl = TRUE)
-
-value_labels[,1] <- gsub("(?<=[0-9][0-9][A-Z])\\'" , "",
-                         value_labels[,1], perl = TRUE,
-                         ignore.case = TRUE)
-
-value_labels[,1] <- gsub("(?<=[0-9][0-9][0-9])\\'" , "",
-                         value_labels[,1], perl = TRUE,
-                         ignore.case = TRUE)
-
-value_labels[,1] <-
-  gsub("\\'(?=[0-9][0-9][0-9]\\')|\\'(?=[0-9][0-9][0-9])" , "",
-       value_labels[,1], perl = TRUE)
-
-
-value_labels[,1] <- gsub("\\'(?=[0-9][A-Z])|\\'(?=[0-9][0-9][A-Z])" , "",
-                         value_labels[,1], perl = TRUE,
-                         ignore.case = TRUE)
-
-value_labels[,1] <- gsub("\\'(?=[A-Z]\\')" , "",
-                         value_labels[,1], perl = TRUE)
-value_labels[,1] <- gsub("(?<= [A-Z])\\'|(?<=^[A-Z])\\'" , "",
-                         value_labels[,1], perl = TRUE,
-                         ignore.case = TRUE)
-
-value_labels[,1] <- gsub("\\'(?=[A-Z][A-Z]\\')" , "",
-                         value_labels[,1], perl = TRUE,
-                         ignore.case = TRUE)
-value_labels[,1] <- gsub("(?<= [A-Z][A-Z])\\'|(?<=^[A-Z][A-Z])\\'" , "",
-                         value_labels[,1], perl = TRUE,
-                         ignore.case = TRUE)
-
-
-value_labels[,1] <- gsub("\\s*\\([^\\)]+\\)", " ",
-                         value_labels[,1])
-
-variable_add_space <- data.frame(codebook_variables$column_number,
-                                 stringsAsFactors = FALSE)
-variable_add_space$added <- paste0(variable_add_space[,1], "   ")
-variable_add_space[,3] <- paste0("^", variable_add_space[,1], " ")
-variable_add_space[,1] <- paste0("^", variable_add_space[,1], "$")
-
-for (i in 1:nrow(variable_add_space)) {
-  value_labels[,1] <- gsub(variable_add_space[i ,1],
-                           variable_add_space[i ,2],
-                           value_labels[,1])
-  value_labels[,1] <- gsub(variable_add_space[i ,3],
-                           variable_add_space[i ,2],
-                           value_labels[,1])
-}
-
-check_length <- data.frame(unlist(strsplit(value_labels[,1],
-                                           split = "\\' |\\'$")),
-                           stringsAsFactors = FALSE)
-if (nrow(check_length) > (1.75 * nrow(value_labels))) {
-  value_labels <- data.frame(unlist(strsplit(value_labels[,1],
-                                             split = "\\' |\\'$")),
-                             stringsAsFactors = FALSE)
-}
-value_labels[,1] <- gsub("\\' ", "\\'    ", value_labels[,1])
-value_labels <- data.frame(unlist(
-  stringr::str_split(value_labels[,1],
-                     "\\s\\s\\s+")), stringsAsFactors = FALSE)
-value_labels[,1] <- gsub("-1", "tempminusone", value_labels[,1])
-value_labels[,1] <- gsub("-2", "tempminustwo", value_labels[,1])
-value_labels[,1] <- gsub("-3", "tempminusthree", value_labels[,1])
-value_labels[,1] <- gsub("-4", "tempminusfour", value_labels[,1])
-value_labels[,1] <- gsub("-5", "tempminusfive", value_labels[,1])
-value_labels[,1] <- gsub("[[:punct:]]", " ", value_labels[,1])
-value_labels[,1] <- gsub("tempminusone", "-1", value_labels[,1])
-value_labels[,1] <- gsub("tempminustwo", "-2", value_labels[,1])
-value_labels[,1] <- gsub("tempminusthree", "-3", value_labels[,1])
-value_labels[,1] <- gsub("tempminusfour", "-4", value_labels[,1])
-value_labels[,1] <- gsub("tempminusfive", "-5", value_labels[,1])
-value_labels[,1] <- gsub("\"", "", value_labels[,1])
-value_labels <- data.frame(value_labels[-grep("^$|^ *$",
-                                              value_labels[,1]),],
-                           stringsAsFactors = FALSE)
-value_labels[,1] <- gsub(' +', ' ', value_labels[,1])
-names(value_labels) <- "column_number"
-value_labels$value <- gsub("(.[:alnum:]*) .*", "\\1", value_labels[,1])
-value_labels$replace <- NA
-for (i in 1:nrow(value_labels)) {
-  value_labels$replace[i] <- gsub(paste0("^",
-                                         value_labels$value[i]), "",
-                                  value_labels$column_number[i])
-}
-replacement <- value_labels[,1][1]
-value_labels[,1] <- gsub(" ", "_", value_labels[,1])
-for (i in 1:nrow(value_labels)) {
-  if (value_labels[i, 1] %in% codebook_variables$column_number) {
-    value_labels[i, 2] <- value_labels[i, 1]
-    value_labels[i, 3] <- ""
-    replacement <- value_labels[i, 1]
-  }
-  value_labels[i, 1] <- replacement
-}
-value_labels <- value_labels[value_labels[,1] != value_labels[,2],]
-value_labels$value <- stringr::str_trim(value_labels$value)
-value_labels <- suppressMessages(dplyr::left_join(value_labels,
-                                                  codebook_column_spaces))
-value_labels <- value_labels[!is.na(value_labels$column_name) &
-                               value_labels$replace != "",]
-value_labels[, c("first_num", "second_num", "column_name")] <- NULL
-
-value_labels <- suppressWarnings(reshape(value_labels,
-                                         timevar = "column_number",
-                                         idvar = "value", direction = "wide"))
-names(value_labels) <- gsub("Replace.", "",
-                            ignore.case = TRUE, names(value_labels))
-
-
-system.time(fix_value_labels(clean_text_file, value_labels))
+# # old code
+#
+#
+# value_labels[,1] <- toupper(value_labels[,1])
+#
+# value_labels[,1] <- gsub('"', "'", value_labels[,1])
+#
+# value_labels[,1] <-  gsub("\\'(?=-1\\')" , "",
+#                           value_labels[,1], perl = TRUE)
+# value_labels[,1] <-  gsub("(?<=-1)\\'" , "",
+#                           value_labels[,1], perl = TRUE)
+#
+#
+# value_labels[,1] <-  gsub("\\'(?=[0-9+]\\')" , "",
+#                           value_labels[,1], perl = TRUE)
+#
+# value_labels[,1] <- gsub("(?<= [0-9])\\' |(?<=^[0-9])\\' " , " ",
+#                          value_labels[,1], perl = TRUE)
+#
+# value_labels[,1] <- gsub("(?<=[0-9][A-Z])\\'" , "",
+#                          value_labels[,1], perl = TRUE,
+#                          ignore.case = TRUE)
+#
+# value_labels[,1] <- gsub("(?<=[0-9][0-9])\\'" , "",
+#                          value_labels[,1], perl = TRUE,
+#                          ignore.case = TRUE)
+# value_labels[,1] <-
+#   gsub("\\'(?=[0-9][0-9]\\')|\\'(?=[0-9][0-9])" , "",
+#        value_labels[,1], perl = TRUE)
+#
+# value_labels[,1] <- gsub("(?<=[0-9][0-9][A-Z])\\'" , "",
+#                          value_labels[,1], perl = TRUE,
+#                          ignore.case = TRUE)
+#
+# value_labels[,1] <- gsub("(?<=[0-9][0-9][0-9])\\'" , "",
+#                          value_labels[,1], perl = TRUE,
+#                          ignore.case = TRUE)
+#
+# value_labels[,1] <-
+#   gsub("\\'(?=[0-9][0-9][0-9]\\')|\\'(?=[0-9][0-9][0-9])" , "",
+#        value_labels[,1], perl = TRUE)
+#
+#
+# value_labels[,1] <- gsub("\\'(?=[0-9][A-Z])|\\'(?=[0-9][0-9][A-Z])" , "",
+#                          value_labels[,1], perl = TRUE,
+#                          ignore.case = TRUE)
+#
+# value_labels[,1] <- gsub("\\'(?=[A-Z]\\')" , "",
+#                          value_labels[,1], perl = TRUE)
+# value_labels[,1] <- gsub("(?<= [A-Z])\\'|(?<=^[A-Z])\\'" , "",
+#                          value_labels[,1], perl = TRUE,
+#                          ignore.case = TRUE)
+#
+# value_labels[,1] <- gsub("\\'(?=[A-Z][A-Z]\\')" , "",
+#                          value_labels[,1], perl = TRUE,
+#                          ignore.case = TRUE)
+# value_labels[,1] <- gsub("(?<= [A-Z][A-Z])\\'|(?<=^[A-Z][A-Z])\\'" , "",
+#                          value_labels[,1], perl = TRUE,
+#                          ignore.case = TRUE)
+#
+#
+# value_labels[,1] <- gsub("\\s*\\([^\\)]+\\)", " ",
+#                          value_labels[,1])
+#
+# variable_add_space <- data.frame(codebook_variables$column_number,
+#                                  stringsAsFactors = FALSE)
+# variable_add_space$added <- paste0(variable_add_space[,1], "   ")
+# variable_add_space[,3] <- paste0("^", variable_add_space[,1], " ")
+# variable_add_space[,1] <- paste0("^", variable_add_space[,1], "$")
+#
+# for (i in 1:nrow(variable_add_space)) {
+#   value_labels[,1] <- gsub(variable_add_space[i ,1],
+#                            variable_add_space[i ,2],
+#                            value_labels[,1])
+#   value_labels[,1] <- gsub(variable_add_space[i ,3],
+#                            variable_add_space[i ,2],
+#                            value_labels[,1])
+# }
+#
+# check_length <- data.frame(unlist(strsplit(value_labels[,1],
+#                                            split = "\\' |\\'$")),
+#                            stringsAsFactors = FALSE)
+# if (nrow(check_length) > (1.75 * nrow(value_labels))) {
+#   value_labels <- data.frame(unlist(strsplit(value_labels[,1],
+#                                              split = "\\' |\\'$")),
+#                              stringsAsFactors = FALSE)
+# }
+# value_labels[,1] <- gsub("\\' ", "\\'    ", value_labels[,1])
+# value_labels <- data.frame(unlist(
+#   stringr::str_split(value_labels[,1],
+#                      "\\s\\s\\s+")), stringsAsFactors = FALSE)
+# value_labels[,1] <- gsub("-1", "tempminusone", value_labels[,1])
+# value_labels[,1] <- gsub("-2", "tempminustwo", value_labels[,1])
+# value_labels[,1] <- gsub("-3", "tempminusthree", value_labels[,1])
+# value_labels[,1] <- gsub("-4", "tempminusfour", value_labels[,1])
+# value_labels[,1] <- gsub("-5", "tempminusfive", value_labels[,1])
+# value_labels[,1] <- gsub("[[:punct:]]", " ", value_labels[,1])
+# value_labels[,1] <- gsub("tempminusone", "-1", value_labels[,1])
+# value_labels[,1] <- gsub("tempminustwo", "-2", value_labels[,1])
+# value_labels[,1] <- gsub("tempminusthree", "-3", value_labels[,1])
+# value_labels[,1] <- gsub("tempminusfour", "-4", value_labels[,1])
+# value_labels[,1] <- gsub("tempminusfive", "-5", value_labels[,1])
+# value_labels[,1] <- gsub("\"", "", value_labels[,1])
+# value_labels <- data.frame(value_labels[-grep("^$|^ *$",
+#                                               value_labels[,1]),],
+#                            stringsAsFactors = FALSE)
+# value_labels[,1] <- gsub(' +', ' ', value_labels[,1])
+# names(value_labels) <- "column_number"
+# value_labels$value <- gsub("(.[:alnum:]*) .*", "\\1", value_labels[,1])
+# value_labels$replace <- NA
+# for (i in 1:nrow(value_labels)) {
+#   value_labels$replace[i] <- gsub(paste0("^",
+#                                          value_labels$value[i]), "",
+#                                   value_labels$column_number[i])
+# }
+# replacement <- value_labels[,1][1]
+# value_labels[,1] <- gsub(" ", "_", value_labels[,1])
+# for (i in 1:nrow(value_labels)) {
+#   if (value_labels[i, 1] %in% codebook_variables$column_number) {
+#     value_labels[i, 2] <- value_labels[i, 1]
+#     value_labels[i, 3] <- ""
+#     replacement <- value_labels[i, 1]
+#   }
+#   value_labels[i, 1] <- replacement
+# }
+# value_labels <- value_labels[value_labels[,1] != value_labels[,2],]
+# value_labels$value <- stringr::str_trim(value_labels$value)
+# value_labels <- suppressMessages(dplyr::left_join(value_labels,
+#                                                   codebook_column_spaces))
+# value_labels <- value_labels[!is.na(value_labels$column_name) &
+#                                value_labels$replace != "",]
+# value_labels[, c("first_num", "second_num", "column_name")] <- NULL
+#
+# value_labels <- suppressWarnings(reshape(value_labels,
+#                                          timevar = "column_number",
+#                                          idvar = "value", direction = "wide"))
+# names(value_labels) <- gsub("Replace.", "",
+#                             ignore.case = TRUE, names(value_labels))
+#
+#
+# system.time(fix_value_labels(clean_text_file, value_labels))
